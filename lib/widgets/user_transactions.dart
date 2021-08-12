1125f5f3bc498e6030880c09a966ee0a63078124
miroj rai <81049151+miroj-lovesmomo@users.noreply.github.com>
2021-08-12 09:28:44 +0545

    Update user_transactions.dart

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -39,7 +39,7 @@ class _UserTransactionsState extends State<UserTransactions> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        NewTransaction(),
+        NewTransaction(_addNewTransaction),
         TransactionList(_userTransacations),
       ],
     );
