40bb3e6218f15d5fab71c514198812c794fc7099
mirojrai <82898308+mirojrai@users.noreply.github.com>
2021-08-15 10:09:30 +0545

    Update chart.dart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,14 +1,30 @@
 import 'package:first_app/Models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 //this is a stateless widget
 class Chart extends StatelessWidget {
   final List<Transaction> recentTranscations;
+  Chart(this.recentTranscations);
   //this is a getter...
   //this is a map...
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
-      return {'Day': 'T', 'Amount': 400};
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTranscations.length; i++) {
+        if (recentTranscations[i].date.day == weekDay.day &&
+            recentTranscations[i].date.month == weekDay.month &&
+            recentTranscations[i].date.month == weekDay.year) {
+          totalSum += recentTranscations[i].amount;
+        }
+      }
+      print(DateFormat.E(weekDay));
+      print(totalSum);
+      return {'Day': DateFormat.E(weekDay), 'Amount': totalSum};
     });
   }
 
