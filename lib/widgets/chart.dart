7e3ccd47cf3f63a32aade0862f97755b17ae1c38
mirojrai <82898308+mirojrai@users.noreply.github.com>
2021-08-15 08:54:39 +0545

    Update chart.dart

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  //this is a getter...
+  //this is a map
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      return {'Day': 'T', 'Amount': 400};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
